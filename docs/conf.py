859911341aa86bd21b9d27ebc0af92582cba8e57
Magdalena Kazmierczak <kazmierczak.magdalena.m@gmail.com>
2024-05-20 12:12:03 +0100

    Trying to add sidebar TOC

--- docs/conf.py
+++ docs/conf.py
@@ -26,3 +26,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'classic'
 html_static_path = ['_static']
+html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'] }
